c17633bda7452f4e9bb136db690d59dd95c96e10
tunglinh-png <tunglinhla91@gmail.com>
2026-09-22 13:31:08 +0700

    Update script URL in banana-thu-hub.lua

--- banana-thu-hub.lua
+++ banana-thu-hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vxezestudio.online/api/scripts/script_yL1RAK4sEz7A/stream/init"))()
+loadstring(game:HttpGet("https://vxezestudio.online/api/scripts/script_VwpjMG2kn7qFw/stream/init"))()
